7f785c676f5b64ca3d04b63ee7d50b54d5b6623b
Jonathan Harris <x-plane@marginal.org.uk>
2014-03-05 20:06:32 +0000

    Split different types of DGSs and jetways into different folders. Cosmetic improvements to Safegate2S model.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ PLUGIN=$(PROJECT)_$(VER).zip
 PLUGIN_FILES=ReadMe.txt Pilot\ Instructions,\ Type\ 2S\ display.pdf $(PROJECT)/alert.wav $(PROJECT)/lin.xpl $(PROJECT)/mac.xpl $(PROJECT)/win.xpl $(PROJECT)/OpenAL32.dll $(PROJECT)/32/win.xpl $(PROJECT)/64/lin.xpl $(PROJECT)/64/win.xpl
 
 PACKAGE=$(PROJECT)_kit_$(VER).zip
-PACKAGE_FILES=AutoGate.html imgs/*.jpeg Jetways/*.dds Jetways/*.png Jetways/*.obj DGSs/*.dds DGSs/*.obj Standalone_DGSs/*.dds Standalone_DGSs/*.obj
+PACKAGE_FILES=AutoGate.html imgs/*.jpeg Helpers/*.dds Helpers/*.png Helpers/*.obj Jetways-Glass/*.dds Jetways-Glass/*.obj Jetways-Steel/*.dds Jetways-Steel/*.obj DGSs-Safedock2S/*.dds DGSs-Safedock2S/*.obj DGSs-Safegate/*.dds DGSs-Safegate/*.obj Standalone-Safedock2S/*.dds Standalone-Safedock2S/*.obj Standalone-Safegate/*.dds Standalone-Safegate/*.obj
 
 INSTALLDIR=~/Desktop/X-Plane\ 10/Resources/plugins
 
@@ -25,5 +25,6 @@ $(PLUGIN):	$(PLUGIN_FILES)
 
 $(PACKAGE):	$(PACKAGE_FILES)
 	./clone.py
+	touch $(PACKAGE_FILES)
 	rm -f $(PACKAGE)
 	zip -MM $(PACKAGE) $(PACKAGE_FILES)
--- clone.py
+++ clone.py
@@ -6,42 +6,39 @@
 refheight=6
 cut=3
 
-for obj in ['Safedock2S-%sm.obj',
-            'Safedock2S-%sm-pole.obj',
-            'Safegate-%sm.obj',
-            'Safegate-%sm-pole.obj']:
-    for height in [3, 3.5, 4, 4.5, 5, 5.5, 6.5, 7, 7.5, 8]:
-        infilename=("DGSs/"+obj) % refheight
-        infile=file(infilename, 'rt')
-        outfilename=("DGSs/"+obj)% height
-        outfile=file(outfilename, 'wt')
-        print outfilename
+for thing in ['Safedock2S', 'Safegate']:
+    for pole in ['','-pole']:
+        for height in [3, 3.5, 4, 4.5, 5, 5.5, 6.5, 7, 7.5, 8]:
+            infilename=("DGSs-%s/%s-%sm%s.obj") % (thing, thing, refheight, pole)
+            infile=file(infilename, 'rt')
+            outfilename=("DGSs-%s/%s-%sm%s.obj") % (thing, thing, height, pole)
+            outfile=file(outfilename, 'wt')
+            print outfilename
 
-        for line in infile:
-            tokens=line.split()
-            if not tokens:
-                outfile.write('\n')
-                continue
-            if tokens[0]=='VT' and float(tokens[2])>cut:
-                outfile.write("VT\t%9.4f %9.4f %9.4f\t%6.3f %6.3f %6.3f\t%-6s %-6s\n" % (
-                    float(tokens[1]),
-                    float(tokens[2])-refheight+height,
-                    float(tokens[3]),
-                    float(tokens[4]),float(tokens[5]),float(tokens[6]),
-                    float(tokens[7]),float(tokens[8])))
-            elif tokens[0]=='ANIM_trans' and float(tokens[2])>cut:
-                #print line
-                outfile.write("%sANIM_trans\t%9.4f %9.4f %9.4f\t%9.4f %9.4f %9.4f\t%s" % (
-                    line.split('A')[0],
-                    float(tokens[1]),
-                    float(tokens[2])-refheight+height,
-                    float(tokens[3]),
-                    float(tokens[4]),
-                    float(tokens[5])-refheight+height,
-                    float(tokens[6]),
-                    line.split(None,7)[-1]))
-            else:
-                outfile.write(line)
+            for line in infile:
+                tokens=line.split()
+                if not tokens:
+                    outfile.write('\n')
+                    continue
+                if tokens[0]=='VT' and float(tokens[2])>cut:
+                    outfile.write("VT\t%9.4f %9.4f %9.4f\t%6.3f %6.3f %6.3f\t%-6s %-6s\n" % (
+                            float(tokens[1]),
+                            float(tokens[2])-refheight+height,
+                            float(tokens[3]),
+                            float(tokens[4]),float(tokens[5]),float(tokens[6]),
+                            float(tokens[7]),float(tokens[8])))
+                elif tokens[0]=='ANIM_trans' and float(tokens[2])>cut:
+                    outfile.write("%sANIM_trans\t%9.4f %9.4f %9.4f\t%9.4f %9.4f %9.4f\t%s" % (
+                            line.split('A')[0],
+                            float(tokens[1]),
+                            float(tokens[2])-refheight+height,
+                            float(tokens[3]),
+                            float(tokens[4]),
+                            float(tokens[5])-refheight+height,
+                            float(tokens[6]),
+                            line.split(None,7)[-1]))
+                else:
+                    outfile.write(line)
 
         outfile.close()
         infile.close()
@@ -49,14 +46,13 @@ for obj in ['Safedock2S-%sm.obj',
 
 refdist=20
 
-for obj in ['SA-%sm-Safedock2S-%sm.obj',
-            'SA-%sm-Safegate-%sm.obj']:
+for thing in ['Safedock2S', 'Safegate']:
     for dist in [16, 18, 20, 22, 24, 26, 28, 30]:
-        for height in [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8]:
+        for height in [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8]:
             if height==refheight and dist==refdist: continue
-            infilename=("Standalone_DGSs/"+obj) % (refdist, refheight)
+            infilename=("Standalone-%s/SA-%sm-%s-%sm.obj") % (thing, refdist, thing, refheight)
             infile=file(infilename, 'rt')
-            outfilename=("Standalone_DGSs/"+obj)% (dist, height)
+            infilename=("Standalone-%s/SA-%sm-%s-%sm.obj") % (thing, dist, thing, height)
             outfile=file(outfilename, 'wt')
             print outfilename
 
